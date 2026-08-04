e035a1d0036cf974a3818799f688854144347428
stesh07-crypto <stesh07@example.com>
2026-08-05 02:24:35 +0300

    Add Swift-ObjC bridging headers for libwdttclient C functions and add ProfilesStore.swift to TunnelExtension target

---
+++ OBhoD/App/OBhoD-Bridging-Header.h
@@ -0,0 +1,6 @@
+#ifndef OBhoD_Bridging_Header_h
+#define OBhoD_Bridging_Header_h
+
+#import "libwdttclient.h"
+
+#endif /* OBhoD_Bridging_Header_h */
---
+++ TunnelExtension/TunnelExtension-Bridging-Header.h
@@ -0,0 +1,6 @@
+#ifndef TunnelExtension_Bridging_Header_h
+#define TunnelExtension_Bridging_Header_h
+
+#import "libwdttclient.h"
+
+#endif /* TunnelExtension_Bridging_Header_h */
