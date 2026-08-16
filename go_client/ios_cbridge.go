cb8cd3819aac17a63807cb9536ee56f0f84b2e77
stesh07-crypto <stesh07@gmail.com>
2026-08-17 01:53:17 +0300

    network core v2: harden packet bridge lifecycle

--- go_client/ios_cbridge.go
+++ go_client/ios_cbridge.go
@@ -1,4 +1,5 @@
 //go:build ios
+
 package main
 
 /*
@@ -13,34 +14,62 @@ static inline void call_write_packet(write_packet_func cb, const void *data, int
 }
 */
 import "C"
+
 import (
+	"sync"
 	"unsafe"
 )
 
 var (
+	packetBridgeMu           sync.RWMutex
 	swiftWritePacketCallback C.write_packet_func
 	packetsFromSwift         = make(chan []byte, 1024)
 )
 
 //export WDTT_SetWriteCallback
 func WDTT_SetWriteCallback(cb C.write_packet_func) {
+	packetBridgeMu.Lock()
 	swiftWritePacketCallback = cb
+	packetBridgeMu.Unlock()
 }
 
 //export WDTT_WritePacket
 func WDTT_WritePacket(data unsafe.Pointer, length C.int) {
+	if data == nil || length <= 0 {
+		return
+	}
 	pkt := C.GoBytes(data, length)
-	buf := make([]byte, length)
+	buf := make([]byte, len(pkt))
 	copy(buf, pkt)
 	select {
 	case packetsFromSwift <- buf:
 	default:
-		// drop packet if channel is full
+		// Backpressure must never block NEPacketTunnelFlow. Dropping here is
+		// preferable to deadlocking the NetworkExtension process.
 	}
 }
 
 func sendPacketToSwift(data []byte) {
-	if swiftWritePacketCallback != nil && len(data) > 0 {
-		C.call_write_packet(swiftWritePacketCallback, unsafe.Pointer(&data[0]), C.int(len(data)))
+	if len(data) == 0 {
+		return
+	}
+	packetBridgeMu.RLock()
+	cb := swiftWritePacketCallback
+	packetBridgeMu.RUnlock()
+	if cb != nil {
+		C.call_write_packet(cb, unsafe.Pointer(&data[0]), C.int(len(data)))
+	}
+}
+
+// Remove packets captured while WireGuard was being recreated. Feeding those
+// stale packets into the new device can otherwise create a burst of obsolete
+// TCP traffic immediately after a Wi-Fi/cellular handoff.
+func drainPacketsFromSwift() {
+	for {
+		select {
+		case <-packetsFromSwift:
+		default:
+			return
+		}
 	}
 }
